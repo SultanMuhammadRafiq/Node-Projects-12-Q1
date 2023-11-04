2983b681641d6b0fb6ea64a41f27fe667c3f1cd3
SultanMuhammad <sultanrafiq1987@gmail.com>
2023-11-04 21:49:47 +0500

    updated

---
+++ Quiz/index.js
@@ -0,0 +1,36 @@
+#!usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+const apiLink = "https://opentdb.com/api.php?amount=11&category=17&difficulty=easy&type=multiple";
+let fetchData = async (data) => {
+    let fetchQuiz = await fetch(data);
+    let res = await fetchQuiz.json();
+    return res.results;
+};
+let data = await fetchData(apiLink);
+let startQuiz = async () => {
+    let score = 0;
+    let name = await inquirer.prompt({
+        type: "input",
+        name: "fName",
+        message: "What is your Name?",
+    });
+    for (let i = 1; i <= 10; i++) {
+        let answers = [...data[i].incorrect_answers, data[i].correct_answer];
+        let ans = await inquirer.prompt({
+            type: "list",
+            name: "quiz",
+            message: data[i].question,
+            choices: answers.map((val) => val),
+        });
+        if (ans.quiz == data[i].correct_answer) {
+            ++score;
+            console.log(chalk.bold.italic.blue("Correct"));
+        }
+        else {
+            console.log(`Correct Answer is ${chalk.bold.italic.red(data[i].correct_answer)} `);
+        }
+    }
+    console.log(`Dear ${chalk.green.bold(name.fName)} , Your Score ${chalk.bold.blue(score)} out of ${chalk.red.bold("10")}`);
+};
+startQuiz();
