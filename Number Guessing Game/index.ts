4a3eada6e2c087044689ce9db747527bc8eea034
SultanMuhammad <sultanrafiq1987@gmail.com>
2023-11-04 21:45:26 +0500

    update

---
+++ Number Guessing Game/index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node 
+
+import inquirer from"inquirer";
+import chalk from"chalk";
+
+
+let genNUmber = Math.floor(Math.random()*2);
+
+let user = await inquirer.prompt({
+    name:"num1",
+    type:"list",
+    message:"Please select number :",
+    choices:["0","1"],
+
+})
+
+if(genNUmber==parseInt(user.num1)){
+    console.log(chalk.bold.green("Congratulations you have won"))
+}
+else
+{
+    console.log(chalk.bold.redBright("Sorry you have Lost, Better Luck Next Time"))
+}
+console.log(user);
