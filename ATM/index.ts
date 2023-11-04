52ab85626bdacca76f4ca8576461de15e14597a9
SultanMuhammad <sultanrafiq1987@gmail.com>
2023-11-04 21:21:40 +0500

    Updated

---
+++ ATM/index.ts
@@ -0,0 +1,90 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import { faker } from "@faker-js/faker";
+
+interface User {
+    id:number
+    pin:number
+    name:string
+    accountNumber:number
+    balance:number 
+}
+
+const createUser =()=>{
+    let users:User[]=[]
+
+    for(let i=1;i<10000;i++){
+        let user:User={
+            id:i,
+            pin:1000+i,
+            name:faker.person.fullName(),
+            accountNumber:Math.floor(10000000*Math.random()*950000),
+            balance:100000*i
+        }
+        users.push(user)
+
+    }
+    return users;
+};
+
+const atmMachine = async(users:User[])=>{
+    const res=await inquirer.prompt({
+        type:"number",
+        message:"Enter your Pin Code",
+        name:"pin"
+    })
+
+    const user = users.find(val=>val.pin==res.pin)
+    if(user){
+        console.log(`Welcome ${user.name}`);
+        atmfunc(user)
+        return;
+    }
+    console.log("Invalid User pin")
+}
+
+const atmfunc=async(user:User)=>{ 
+    const ans=await inquirer.prompt({
+        type:"list",
+        name:"select",
+        message:"Select the option...",
+        choices:["Withdraw","Balance ","transfer","Exit"]
+
+    })
+   
+
+    if(ans.select=="Withdraw"){
+        const amount=await inquirer.prompt({
+            type:"number",
+            message:"Enter Amount",
+            name:"Pkr"
+        })
+        if(amount.Pkr>user.balance){
+            return console.log("Account Balance is not sufesficient to fulfill your request")
+        }
+        console.log(`Withdraw Amount:${amount.Pkr}`)
+        console.log(`Balance:${user.balance-amount.Pkr}`)
+    }
+    if(ans.select=="Balance"){
+        console.log(`Balance: ${user.balance} `)
+        return
+    }
+    if(ans.select=="transfer"){
+        const transfer=await inquirer.prompt({
+            type:"number",
+            message:"Enter your amount here",
+            name:"PKR"
+        })
+        console.log(`Transfer Amount:${transfer.PKR}`)
+        console.log(`Total Balance:${user.balance+transfer.PKR}`)
+
+    }
+    if(ans.select=="Exit"){
+        console.log("Thanks for using ATM")
+    }
+}
+
+const users = createUser();
+atmMachine(users);
+
